d27a9a9a51d481d7d090b5f7c6a76f0c652fde5f
takumi7761995 <takumi7761995@gmail.com>
2022-04-10 21:57:58 +0900

    Initial Commit

---
+++ GenericsSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GenericsSample
+//
+//  Created by 杉本匠 on 2022/04/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GenericsSample/GenericsSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  GenericsSampleApp.swift
+//  GenericsSample
+//
+//  Created by 杉本匠 on 2022/04/10.
+//
+
+import SwiftUI
+
+@main
+struct GenericsSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
